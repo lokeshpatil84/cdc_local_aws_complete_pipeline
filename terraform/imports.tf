598bd7fd3041227dea827a86dcbad9d223d3fff3
Lokesh Patil <lokeshp6@L-18082L.kpit.com>
2026-02-07 00:27:51 +0530

    remove auto deploy logic from all branch

--- terraform/imports.tf
+++ terraform/imports.tf
@@ -1,11 +1,7 @@
 # This file handles the import of existing resources to prevent "ResourceExistsException"
 # Terraform 1.5+ allows import blocks directly in configuration.
 
-# Replace 'YOUR_ACCOUNT_ID' with your actual AWS Account ID
-# This tells Terraform to adopt the existing secret into the state
-
 import {
   to = aws_secretsmanager_secret.db_credentials
-  # IMPORTANT: The ARN must include the 6-character random suffix (e.g., -xYz123)
   id = "arn:aws:secretsmanager:ap-south-1:661722818226:secret:cdc-pipeline-prod-db-credentials-sXAK91"
 }
